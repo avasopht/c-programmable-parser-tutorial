e6ac6ad12f0db826b99841bc5dd86e6144b81759
Keldon Alleyne <keldon.alleyne@avasopht.com>
2020-03-18 20:56:11 +0000

    Automatic GNU style formatting in CLion applied to parser.

--- parser.c
+++ parser.c
@@ -50,8 +50,6 @@
 
 #include <stdbool.h>
 
-
-
 typedef enum RefType RefType;
 typedef struct Ref Ref;
 typedef struct List List;
@@ -59,7 +57,7 @@ typedef struct ParseResult ParseResult;
 
 /* Parse functions can accept arguments, such as the range of characters
    in :character-range */
-typedef ParseResult *(*ParseFn)(const char *text, List *args);
+typedef ParseResult *(*ParseFn) (const char *text, List *args);
 
 /** Each parser returns NULL on failure or a parse result containing
     a pointer to the text that follows it, and its parse tree.
@@ -69,38 +67,46 @@ typedef ParseResult *(*ParseFn)(const char *text, List *args);
     `(:character "t")`
  */
 
-enum RefType
-  {
-   REF_INT,
-   REF_LITERAL,
-   REF_LIST,
-   REF_PARSER
-  };
-
-
-struct Ref
-{
-  RefType type;
-  union
-  {
-    List *list;
-    const char *literal;
-    int integer;
-    ParseFn parser;
-  };
+enum RefType {
+    REF_INT,
+    REF_LITERAL,
+    REF_LIST,
+    REF_PARSER
 };
 
-struct List
-{
-  List *next;
-  Ref *data;
+struct Ref {
+    RefType type;
+    union {
+        List *list;
+        const char *literal;
+        int integer;
+        ParseFn parser;
+    };
 };
 
-struct ParseResult
-{
-  const char *remaining;
-  List *tree;
+/**
+ * A Ref instance is used by List to hold its data (which may hold an integer,
+ * literal string, list, or a parser).
+ */
+
+struct List {
+    List *next;
+    Ref *data;
 };
+
+struct ParseResult {
+    const char *remaining;
+    List *tree;
+};
+
+/**
+ * A parse result holds a pointer to the text that follows the portion of
+ * text it has parsed.
+ *
+ * For example, a number parser given "123abc" would return a parse tree
+ * for "123", and a pointer to the remaining "abc", allowing parsers to
+ * be chained together.
+ */
 
 /** These are the key structures we will need for parsing. They are pretty
     self explanatory, but pay special attention to the `data` member in `List`,
@@ -112,80 +118,100 @@ struct ParseResult
 #include <string.h>
 #include <stdio.h>
 
-bool is_literal_list(List *l)
+bool list_data_is_literal (List *l)
 {
   assert(l && l->data);
-  return l->data->type == REF_LITERAL;
+  return l->data->type == REF_LITERAL && l->data->literal;
 }
 
-List *make_list()
+List *make_list ()
 {
-  List *l = calloc(1, sizeof(List));
+  List *l = calloc (1, sizeof (List));
+  assert(l);
   return l;
 }
 
-Ref *make_ref(RefType type)
+Ref *make_ref (RefType type)
 {
-  Ref *ref = calloc(1, sizeof(List));
+  Ref *ref = calloc (1, sizeof (List));
+  assert(ref);
   ref->type = type;
   return ref;
 }
 
-Ref *make_literal(const char *literal)
+Ref *make_literal (const char *literal)
 {
-  Ref *ref = make_ref(REF_LITERAL);
+  Ref *ref = make_ref (REF_LITERAL);
   ref->literal = literal;
   return ref;
 }
 
-bool streq(const char *first, const char *second)
+bool streq (const char *first, const char *second)
 {
-  return strcmp(first, second) == 0;
+  return strcmp (first, second) == 0;
 }
 
-ParseResult *make_result()
+ParseResult *make_result ()
 {
-  ParseResult *result = calloc(1, sizeof(ParseResult));
+  ParseResult *result = calloc (1, sizeof (ParseResult));
   return result;
 }
 
-ParseResult *parse_char(const char *in, List *character)
+ParseResult *parse_single_char (const char *in, List *character_param)
 {
-  assert(is_literal_list(character));
-  assert(!character->next);
-  assert(character->data->literal);
-  assert(strlen(character->data->literal) > 0);
-  char ch = character->data->literal[0];
-  if(in[0] != ch)
+  /**
+   * parse_single_char demonstrates the simplest of parsers that accepts
+   * parses a single character_param.
+   *
+   * There is a lot of code involved for such a simple process, but most of
+   * your parsers are going to be written in s-expressions that can do away
+   * with all of the boilerplate.
+   */
+
+  assert(list_data_is_literal (character_param));
+  assert(!character_param->next);
+  assert(strlen (character_param->data->literal) > 0);
+  /**
+   * These asserts declare that character_param is a string literal with at
+   * least one character, such as ("abcdef").
+   */
+
+  char ch = character_param->data->literal[0];
+  const char *in_ptr = in;
+  if (*in_ptr++ != ch)
     {
       return NULL;
     }
-  ParseResult *result = make_result();
-  result->remaining = &in[1];
-  List *tree = make_list();
-  result->tree = tree;
-  
-  tree->data = make_literal(":character");
-  List *char_list = make_list();
-  char *char_str = calloc(2, sizeof(char));
-  char_str[0] = ch;
-  char_list->data = make_literal(char_str);
-  tree->next = char_list;
+
+  // Return parse tree result (:character "t")
+  ParseResult *result = make_result ();
+  {
+    result->remaining = in_ptr;
+    List *tree = make_list ();
+    result->tree = tree;
+
+    tree->data = make_literal (":character");
+    List *char_list = make_list ();
+    char *char_str = calloc (2, sizeof (char));
+    char_str[0] = ch;
+    char_list->data = make_literal (char_str);
+    tree->next = char_list;
+  }
   return result;
 }
 
-void test_parse_char()
+void test_parse_char ()
 {
-  List *char_args = make_list();
-  char_args->data = make_literal("t");
-  ParseResult *result = parse_char("two", char_args);
-  assert(streq(":character", result->tree->data->literal));
-  assert(streq("t", result->tree->next->data->literal));
-  assert(streq("wo", result->remaining));
-  printf("PASSED::test_parse_char\n");
+  List *char_args = make_list ();
+  char_args->data = make_literal ("t");
+  ParseResult *result = parse_single_char ("two", char_args);
+  assert(streq (":character", result->tree->data->literal));
+  assert(streq ("t", result->tree->next->data->literal));
+  assert(streq ("wo", result->remaining));
+  printf ("PASSED::test_parse_char\n");
 }
 
-int main()
+int main ()
 {
-  test_parse_char();
+  test_parse_char ();
 }
